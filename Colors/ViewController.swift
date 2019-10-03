7c465200de564832c88074cb2491b5a4524cb78d
grantharrison <gthgg6@mail.missouri.edu>
2019-10-02 19:51:04 -0500

    Initial Commit

---
+++ Colors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Colors
+//
+//  Created by Grant Harrison on 10/2/19.
+//  Copyright © 2019 Grant Harrison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
